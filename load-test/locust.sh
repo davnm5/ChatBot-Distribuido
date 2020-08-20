c9fd7ca4962efe015ae5d3c290d8e38d6b8a2381
David Neira <josdanei@espol.edu.ec>
2020-08-20 12:50:22 -0500

    Update locust.sh

--- load-test/locust.sh
+++ load-test/locust.sh
@@ -1,4 +1,4 @@
 sudo docker run -d -p 8089:8089 \
--e LOCUST_LOCUSTFILE_URL='https://example.com/locustfile.py' \
+-e LOCUST_LOCUSTFILE_URL='https://github.com/davnm5/ChatBot-Distribuido/blob/master/load-test/locustfile.py' \
 -e LOCUST_TARGET_HOST='http://pc776.emulab.net:30001/chat' \
 --name locust peterevans/locust:latest
